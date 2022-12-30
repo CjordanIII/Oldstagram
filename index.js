de75e565216270718cac91ae1a9f41e219f5b74e
CjordanIII <118429283+CjordanIII@users.noreply.github.com>
2022-12-30 18:44:23 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 let main = document.getElementById("main")
-
+let head = document.getElementById("head")
+let btn1 = document.getElementById("like-btn")
 
 
 const posts = [
@@ -16,8 +17,8 @@ const posts = [
         name: "Gustave Courbet",
         username: "gus1819",
         location: "Ornans, France",
-        avatar: "images/avatar-courbet.jpg",
-        post: "images/post-courbet.jpg",
+        avatar: "photos/avatar-courbet.jpg",
+        post: "photos/post-courbet.jpg",
         comment: "i'm feelin a bit stressed tbh",
         likes: 4
     },
@@ -25,70 +26,86 @@ const posts = [
         name: "Joseph Ducreux",
         username: "jd1735",
         location: "Paris, France",
-        avatar: "images/avatar-ducreux.jpg",
-        post: "images/post-ducreux.jpg",
+        avatar: "photos/avatar-ducreux.jpg",
+        post: "photos/post-ducreux.jpg",
         comment: "gm friends! which coin are YOU stacking up today?? post below and WAGMI!",
         likes: 152
     }
 ]
 
-function images(){
-    return posts[0].avatar
-}
-
-function names(){
-    return posts[0].name
-}
-function locations(){
-    return posts[0].location
-}
-
-function post(){
-    return posts[0].post
-}
-function likes(){
-    return posts[0].likes
-} 
-function comments(){
-    return posts[0].comment
-}
-
-function username2(){
-    return posts[0].username
-}
 
 
-
-main.innerHTML +=
+head.innerHTML =
 `<section class="container-1">
-    <img src="/photos/oldagram-logo.svg" id="logo" alt="Logo"/>
-    <img src="/photos/person-circle.svg" class="profile-icon"/>
+<img src="/photos/oldagram-logo.svg" id="logo" alt="Logo"/>
+<img src="/photos/person-circle.svg" class="profile-icon"/>
 
-</section>
+</section>`
 
-<section>
-    <img class="img-avatar" src=${images()} alt = avatar of ${names()}  />
-    <p id="username">${names()}</p>
-    <p id ="location">${locations()}
-</section>
-<section>
-    <img id="profile-pic" alt="vango post" src="${post()}"/>
-</section>
-<section class="container-2">
-    <img id=" like" alt="like" src="photos/like.svg" />
-    <img id=" coment" alt="coment" src="photos/comment.svg" />
-    <img id=" share" alt="share" src="photos/share.svg" />
-</section>
-<section class="likes">
-    <p>${likes()} likes</p>
-</section>
-<section class="comment">
-    <p><span id="comment-username">${username2()}</span> ${comments()}</p>
-</section>
-<br>
+function render(){
 
+        main.innerHTML +=
+    `<section>
+        <img class="img-avatar" src=${images()} alt = avatar of ${names()}  />
+        <p id="username">${names()}</p>
+        <p id ="location">${locations()}
+    </section>
+    <section>
+        <img id="profile-pic" alt="post" src="${post()}"/>
+    </section>
+    <section class="container-2">
+        <button id="like-btn" Onclick="toggle1()"><img id=" like" alt="liked" src="photos/like.svg" /></button>
+        <button id="comment-btn"><img id=" coment" alt="coment" src="photos/comment.svg" /></button>
+        <button id="share-btn"><img id=" share" alt="share" src="photos/share.svg" /></button>
+    </section>
+    <section class="likes">
+        <p>${likes()} likes</p>
+    </section>
+    <section class="comment">
+        <p><span id="comment-username">${username2()}</span> ${comments()}</p>
+    </section>
+    <div class= "line" >
+    </div>
+    `
+}
 
+for(let i = 0; i<posts.length; i++){
+    function images(){
+        return posts[i].avatar
+    }
+    function names(){
+        return posts[i].name
+    }
+    function locations(){
+        return posts[i].location
+    }
+    
+    function post(){
+        return posts[i].post
+    }
+    function likes(){
+        return posts[i].likes
+    } 
+    function comments(){
+        return posts[i].comment
+    }
+    
+    function username2(){
+        return posts[i].username
+    }
+ 
+    
+    render()
 
+}
 
+/*    function toggle1(){
+    if(btn1.style.color=="red"){
+        btn1.style.color="grey"
+    }
+    else{
+        btn1.style.color="red"
+    }
+} 
+ */
 
-`
